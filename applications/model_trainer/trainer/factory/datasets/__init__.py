adb49756723f17c089b0f3018f5002de98c8a661
gatheluck <gatheluck+tech@gmail.com>
2021-08-11 02:10:57 +0900

    feat: use albumentation to dataset

---
+++ applications/model_trainer/trainer/factory/datasets/__init__.py
@@ -0,0 +1,93 @@
+import pathlib
+from dataclasses import dataclass
+from typing import Final, Tuple
+
+import torch
+import pytorch_lightning as pl
+from omegaconf import MISSING
+from torch.utils.data import DataLoader, Dataset
+
+
+@dataclass(frozen=True)
+class DatasetStats:
+    num_classes: int = MISSING
+    input_size: int = MISSING
+    mean: Tuple[float, float, float] = MISSING
+    std: Tuple[float, float, float] = MISSING
+
+
+class BaseDataModule(pl.LightningDataModule):
+    """Base class for all 2d image LightningDataModule.
+    A datamodule encapsulates the five steps involved in data processing in PyTorch:
+    - Download / tokenize / process.
+    - Clean and (maybe) save to disk.
+    - Load inside Dataset.
+    - Apply transforms (rotate, tokenize, etc…).
+    - Wrap inside a DataLoader.
+    For more detail, please check official docs: https://pytorch-lightning.readthedocs.io/en/stable/datamodules.html#what-is-a-datamodule
+
+    Attributes:
+        batch_size (int): The size of input image.
+        num_workers (int): The number of workers.
+        dataset_stats (DatasetStats): The dataclass which holds dataset statistics.
+        train_dataset: (Dataset): The dataset for training.
+        val_dataset: (Dataset): The dataset for validation.
+
+    """
+
+    def __init__(self, batch_size: int, num_workers: int, root: pathlib.Path) -> None:
+        super().__init__()
+        self.batch_size: Final[int] = batch_size
+        self.num_workers: Final[int] = num_workers
+        self.dataset_stats: DatasetStats
+        self.train_dataset: Dataset
+        self.val_dataset: Dataset
+        self.test_dataset: Dataset
+
+    def prepare_data(self, *args, **kwargs) -> None:
+        """Use this method to do things that might write to disk or that need to be done only from a single GPU in distributed settings."""
+        raise NotImplementedError()
+
+    def setup(self, stage=None, *args, **kwargs) -> None:
+        """There are also data operations you might want to perform on every GPU."""
+        raise NotImplementedError()
+
+    def train_dataloader(self, *args, **kwargs) -> DataLoader:
+        return DataLoader(
+            self.train_dataset,
+            self.batch_size,
+            shuffle=True,
+            num_workers=self.num_workers,
+        )
+
+    def val_dataloader(self, *args, **kwargs) -> DataLoader:
+        return DataLoader(
+            self.val_dataset,
+            self.batch_size,
+            shuffle=False,
+            num_workers=self.num_workers,
+        )
+
+    def test_dataloader(self, num_samples: int = -1, *args, **kwargs) -> DataLoader:
+        return DataLoader(
+            self.val_dataset,
+            self.batch_size,
+            shuffle=False,
+            num_workers=self.num_workers,
+        )
+
+    def _get_subset(self, dataset: Dataset, num_samples: int) -> Dataset:
+        if num_samples != -1:
+            num_samples = min(num_samples, len(dataset))  # type: ignore
+            indices = [i for i in range(num_samples)]
+            return torch.utils.data.Subset(dataset, indices)
+        else:
+            return dataset
+
+    @property
+    def num_classes(self) -> int:
+        return self.dataset_stats.num_classes
+
+    @property
+    def input_size(self) -> int:
+        return self.dataset_stats.input_size
---
+++ applications/model_trainer/trainer/factory/datasets/cifar10.py
@@ -0,0 +1,115 @@
+import pathlib
+from dataclasses import dataclass
+from typing import Any, Callable, Final, Optional, Tuple
+
+import albumentations as albu
+import torchvision
+from albumentations.pytorch import ToTensorV2
+from torch.utils.data import Dataset
+
+from trainer.factory.datasets import BaseDataModule, DatasetStats
+
+
+@dataclass(frozen=True)
+class Cifar10Stats(DatasetStats):
+    num_classes: int = 10
+    input_size: int = 32
+    mean: Tuple[float, float, float] = (0.49139968, 0.48215841, 0.44653091)
+    std: Tuple[float, float, float] = (0.24703223, 0.24348513, 0.26158784)
+
+
+class CIAFR10(torchvision.datasets.CIFAR10):
+    def __init__(
+            self,
+            root: str,
+            train: bool = True,
+            transform: Optional[Callable] = None,
+            download: bool = False,
+    ):
+        super().__init__(
+            root=root,
+            train=train,
+            transform=None,
+            target_transform=None,
+            download=download,
+        )
+        self.transform = transform
+
+    def __getitem__(self, index: int) -> Tuple[Any, Any]:
+        """
+        Args:
+            index (int): Index
+        Returns:
+            tuple: (image, target) where target is index of the target class.
+        """
+        img, target = self.data[index], self.targets[index]
+
+        if self.transform:
+            img = self.transform(image=img)["image"]
+
+        return img, target
+
+
+class Cifar10DataModule(BaseDataModule):
+    """The LightningDataModule for CIFAR-10 dataset.
+
+    Attributes:
+        dataset_stats (DatasetStats): The dataclass which holds dataset statistics.
+        root (pathlib.Path): The root path which dataset exists. If not, try to download.
+
+    """
+
+    def __init__(self, batch_size: int, num_workers: int, root: pathlib.Path) -> None:
+        super().__init__(batch_size, num_workers, root)
+        self.dataset_stats: DatasetStats = Cifar10Stats()
+        self.root: Final[pathlib.Path] = root / "cifar10"
+
+    def prepare_data(self, *args, **kwargs) -> None:
+        """Try to download dataset (DO NOT assign train/val here)."""
+        self.root.mkdir(exist_ok=True, parents=True)
+        torchvision.datasets.CIFAR10(root=self.root, train=True, download=True)
+        torchvision.datasets.CIFAR10(root=self.root, train=False, download=True)
+
+    def setup(self, stage=None, *args, **kwargs) -> None:
+        """Assign test dataset """
+        self.train_dataset: Dataset = CIAFR10(
+            root=self.root,
+            train=True,
+            transform=self._get_transform(train=True),
+            download=True,
+        )
+
+        self.val_dataset: Dataset = CIAFR10(
+            root=self.root,
+            train=False,
+            transform=self._get_transform(train=False),
+            download=True,
+        )
+
+    def _get_transform(
+            self,
+            train: bool,
+            normalize: bool = True,
+    ) -> albu.Compose:
+        transform = list()
+
+        input_size: Final = self.input_size
+        mean: Final = self.dataset_stats.mean
+        std: Final = self.dataset_stats.std
+
+        if train:
+            transform.extend(
+                [
+                    albu.augmentations.transforms.HorizontalFlip(p=0.5),
+                    albu.augmentations.transforms.PadIfNeeded(min_height=input_size + 4, min_width=input_size + 4),
+                    albu.augmentations.crops.transforms.RandomCrop(height=input_size, width=input_size, p=1.0),
+                ]
+            )
+        else:
+            pass
+
+        if normalize:
+            transform.extend([albu.augmentations.transforms.Normalize(mean=mean, std=std)])
+
+        transform.extend([ToTensorV2()])
+        return albu.Compose(transform)
